969b21d9da7246d14c02f02a46f94b5d33166d23
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-08-30 04:32:56 +0300

    More rspec style tests

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -3,9 +3,14 @@ require 'rspec_api_documentation/dsl'
 
 resource "Users" do
   get "/users" do
-    example "Listing users" do
+    it "responds with 200" do
       do_request
       expect(status).to be 200
+    end
+
+    it "includes users" do
+      do_request
+      expect(response_body).to eq({"users" => []}.to_json)
     end
   end
 end
