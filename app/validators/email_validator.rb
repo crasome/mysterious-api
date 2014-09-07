65464810ec889ed4795da639e527f2f433b5d1b2
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-09-08 02:32:13 +0300

    Add EmailValidator

---
+++ app/validators/email_validator.rb
@@ -0,0 +1,11 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless valid? value
+      record.errors.add attribute, (options[:message] || "should be a valid email address so we can contact you back")
+    end
+  end
+
+  def valid?(email)
+    email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  end
+end
---
+++ spec/validators/email_validator_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+require "active_model"
+require "validators/email_validator"
+
+# TODO: add more examples in live app
+describe EmailValidator do
+  let(:validator) { described_class.new double.as_null_object }
+
+  it "passes for valid emails" do
+    %w[test@example.com freddie.mercury@queen.com].each do |email|
+      expect(validator).to be_valid email
+    end
+  end
+
+  it "disallow invalids" do
+    %w[invalid_email alice@1^3.com].each do |email|
+      expect(validator).not_to be_valid email
+    end
+  end
+end
+
