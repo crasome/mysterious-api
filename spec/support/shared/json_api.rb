e60ef44ccb25bc083ffbc359b62e7aac2fc24303
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-09-08 02:37:58 +0300

    :wrench:

--- spec/support/shared/json_api.rb
+++ spec/support/shared/json_api.rb
@@ -4,19 +4,19 @@ shared_examples_for :json_api_collection do
     expect(json_response).to have_key resource_name
   end
 
-  it "represented as array" do
+  it "is represented as array" do
     do_request default_params
     expect(json_response[resource_name]).to be_an Array
   end
 end
 
 shared_examples_for :json_api_resource do
-  it "has users key" do
+  it "has resource name key" do
     do_request default_params
     expect(json_response).to have_key resource_name
   end
 
-  it "represented as hash" do
+  it "is represented as hash" do
     do_request default_params
     expect(json_response[resource_name]).to be_a Hash
   end
