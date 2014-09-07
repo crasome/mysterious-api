0463585be460e8f3ab442e90b5879c334f9b140c
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-09-08 00:56:27 +0300

    Add Json API shared specs

--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -3,13 +3,11 @@ require 'rspec_api_documentation/dsl'
 
 resource "Users" do
   get "/users" do
-
     include_context :json
-    it_should_behave_like :ok_request
 
-    it "has users key" do
-      do_request
-      expect(json_response).to have_key :users
+    it_behaves_like :ok_request
+    it_behaves_like :json_api_collection do
+      let(:resource_name) { :users }
     end
 
     it "includes users" do
@@ -19,6 +17,16 @@ resource "Users" do
       users.each do |user|
         expect(json_response[:users]).to include include(email: user.email)
       end
+    end
+  end
+
+  get "/users/:id" do
+    include_context :json
+    let(:id) { user.id }
+
+    it_behaves_like :ok_request
+    it_behaves_like :json_api_resource do
+      let(:resource_name) { :users }
     end
   end
 end
---
+++ spec/support/shared/json_api.rb
@@ -0,0 +1,19 @@
+shared_examples_for :json_api_collection do
+  example_request "has resource name key" do
+    expect(json_response).to have_key resource_name
+  end
+
+  example_request "represented as array" do
+    expect(json_response[resource_name]).to be_an Array
+  end
+end
+
+shared_examples_for :json_api_resource do
+  example_request "has users key" do
+    expect(json_response).to have_key resource_name
+  end
+
+  example_request "represented as hash" do
+    expect(json_response[resource_name]).to be_an Array
+  end
+end
