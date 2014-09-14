357cc85d30c2f1d8abdd6d6115c65951beb6617f
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-09-14 18:56:29 +0300

    Move acceptance helpers to acceptance_helpers directory

--- spec/support/acceptance_helpers/requests.rb
+++ spec/support/acceptance_helpers/requests.rb
@@ -4,6 +4,12 @@ shared_examples_for :ok_request do
   end
 end
 
+shared_examples_for :created_request do
+  example_request "has status 201" do
+    expect(status).to eq 201
+  end
+end
+
 shared_examples_for :invalid_attributes_request do
   example_request "has status 422" do
     expect(status).to eq 422
