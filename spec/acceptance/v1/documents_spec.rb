786f9d34bcb8675da423296d5c4c2b596c6c3eba
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-09-14 16:27:38 +0300

    Validate response type and authorization of /documents

--- spec/acceptance/v1/documents_spec.rb
+++ spec/acceptance/v1/documents_spec.rb
@@ -18,5 +18,20 @@ resource "Documents" do
         )
       end
     end
+
+    describe "response", :no_doc do
+      include_context :json
+      include_examples :ok_request
+      it_behaves_like :json_api_collection do
+        let(:resource_name) { :documents }
+      end
+    end
+
+    describe "authorization", :no_doc do
+      it_behaves_like :publicly_accessible_request
+      it_behaves_like :restricted_request, rejected_roles: []  do
+        let(:resource) { documents }
+      end
+    end
   end
 end
