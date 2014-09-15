d44bd8753b68e981d9ed2fd874e7fe5f5b0450ee
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2014-09-15 20:51:58 +0300

    Extract permission specs to shared_examples

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,33 +1,14 @@
 require 'rails_helper'
-require 'pundit/rspec'
 require 'matrix'
+require_relative "shared_examples/permissin_matrix_specs"
 
 describe UserPolicy do
   subject { described_class }
   let(:resource) { build_stubbed :user }
 
-  permission_matrix = Matrix[
+  include_examples :permission_matrix, matrix: Matrix[
     [nil,       :owner,  :admin,  :registered,  :guest],
     [:show?,    true,    true,    true,         true  ],
     [:update?,  true,    true,    false,        false ],
   ]
-
-  actions           = permission_matrix.column_vectors[0][1..-1]
-  roles             = permission_matrix.row_vectors[0][1..-1]
-  permission_values = permission_matrix.minor(1..-1, 1..-1)
-
-  actions.each_with_index do |action, action_index|
-    permissions action do
-      roles.each_with_index do |role, role_index|
-        permission = permission_values[action_index, role_index]
-        it "is #{permission ? "granted" : "denied"} to #{role}" do
-          user = build_stubbed :user, role, resource: resource
-
-          expect(subject).send(permission ? :to : :not_to, permit(
-            user, resource
-          ))
-        end
-      end
-    end
-  end
 end
