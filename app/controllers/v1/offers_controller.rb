58de50c9752a8f9dfe520e23cb5fb8e24392a4f0
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2015-07-26 21:03:52 +0300

    Render offers in OffersController

--- app/controllers/v1/offers_controller.rb
+++ app/controllers/v1/offers_controller.rb
@@ -1,6 +1,17 @@
 module V1
   class OffersController < ApiController
     def index
+      load_offers
+      render json: @offers
+    end
+
+    private
+    def load_offers
+      @offers = fyber.get_offers **offer_request_params
+    end
+
+    def offer_request_params
+      params.permit(:uid, :pub0, :page)
     end
   end
 end
