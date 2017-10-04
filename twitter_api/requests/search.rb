cc62ec7c5fb3650814cc60e9ba4bb7d27d459315
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2017-10-05 01:04:07 +0300

    Encode params

--- twitter_api/requests/search.rb
+++ twitter_api/requests/search.rb
@@ -12,12 +12,16 @@ module TwitterApi
       end
 
       def perform
-        client.search(query, result_type: "recent").take(limit)
+        client.search(encoded_query, result_type: "recent").take(limit)
       end
 
       private
 
       attr_reader :query, :client, :limit
+
+      def encoded_query
+        @encoded_query ||=  URI.encode_www_form_component(query)
+      end
     end
   end
 end
